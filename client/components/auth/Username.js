b6ba54be57859322f1293096e06eb8082df9d192
simon-soen <in160005620@kisiiuniversity.ac.ke>
2023-12-28 12:13:59 +0300

    api working now

---
+++ client/components/auth/Username.js
@@ -0,0 +1,26 @@
+// apiUtils.js
+const fetchSupplierUsername = async (item) => {
+    try {
+      if (!item || !item.supplier) {
+        console.error('Supplier information is missing in the item object');
+        return 'Unknown';
+      }
+  
+      const response = await fetch(`http://localhost:3000/api/users/${item.supplier.replace(/"/g, '')}`);
+      const userData = await response.json();
+  
+      if (response.ok) {
+        console.log('Supplier username:', userData.username);
+        return userData.username;
+      } else {
+        console.log('Server returned an error:', userData.error);
+        return 'Unknown';
+      }
+    } catch (error) {
+      console.error('Error fetching supplier details:', error);
+      return 'Unknown';
+    }
+  };
+  
+  export { fetchSupplierUsername };
+  
