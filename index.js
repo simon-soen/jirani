f7574392bac0a6b47708bedaa320af68935a1840
simon-soen <in160005620@kisiiuniversity.ac.ke>
2023-12-28 13:47:00 +0300

    api

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
 const app = express();
 const productRouter = require('./routes/products');
 const authRouter = require('./routes/auth');
@@ -10,15 +9,10 @@ const userRouter = require('./routes/user');
 const cartRouter = require('./routes/cart');
 const orderRouter = require('./routes/order');
 const favouriteRouter = require('./routes/favourite');
-const upload = require("./routes/upload")
-const port = 8080;
-const path = require('path');
+const port = 3000;
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('db connected')).catch((err) => console.log(err));
-
-
-
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
@@ -27,14 +21,13 @@ app.use(express.static(path.join(__dirname+ '/client/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+ '/client/dist/index.html'));
 });
-
-app.use('api/products', productRouter);
+app.use('/api/products', productRouter);
 app.use('/api/', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
 app.use('/api/favourite', favouriteRouter);
-app.use("/api/file", upload)
+
 
 
 app.listen(process.env.PORT || port, () =>
