9399b8263e65fac31db3b9451339119abee2a4a6
Desmond Mwirigi <104234503+Desmo-nd@users.noreply.github.com>
2024-02-06 18:12:06 +0300

    Update Carousel.jsx

--- client/components/home/Carousel.jsx
+++ client/components/home/Carousel.jsx
@@ -7,7 +7,8 @@ const Carousel = () => {
     const slides = [
         "https://d326fntlu7tb1e.cloudfront.net/uploads/cb2e64a8-ad4c-4d45-b58b-b0c7e11b6bb4-fn1.jpg", 
         "https://d326fntlu7tb1e.cloudfront.net/uploads/b1f6d96d-3297-4270-ba65-657dc2bc0236-fn2.jpg", 
-        "https://bucketeer-897a58fa-5a33-4dbf-aa4a-7ab2e1c7ea29.s3.us-east-1.amazonaws.com/products/1706948762619-998050be-6100-4cfe-a7db-4fb0a506b3ff.jpeg"
+        "https://bucketeer-897a58fa-5a33-4dbf-aa4a-7ab2e1c7ea29.s3.us-east-1.amazonaws.com/products/1706948762619-998050be-6100-4cfe-a7db-4fb0a506b3ff.jpeg",
+        "https://bucketeer-897a58fa-5a33-4dbf-aa4a-7ab2e1c7ea29.s3.us-east-1.amazonaws.com/products/1706955110795-4de8daab-cb34-4310-9b7a-2b1b7507edc4.jpeg"
 
 
     ]
