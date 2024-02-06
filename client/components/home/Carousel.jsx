5b6ea2bdbb9b759fe114f54ea5af79fbdc07d483
Desmond Mwirigi <104234503+Desmo-nd@users.noreply.github.com>
2024-02-06 18:10:38 +0300

    Update Carousel.jsx

--- client/components/home/Carousel.jsx
+++ client/components/home/Carousel.jsx
@@ -7,7 +7,7 @@ const Carousel = () => {
     const slides = [
         "https://d326fntlu7tb1e.cloudfront.net/uploads/cb2e64a8-ad4c-4d45-b58b-b0c7e11b6bb4-fn1.jpg", 
         "https://d326fntlu7tb1e.cloudfront.net/uploads/b1f6d96d-3297-4270-ba65-657dc2bc0236-fn2.jpg", 
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/5d445b91-c01a-4564-8ff8-c27c2b88ea5b-fn7.png",
+        "https://bucketeer-897a58fa-5a33-4dbf-aa4a-7ab2e1c7ea29.s3.us-east-1.amazonaws.com/products/1706948762619-998050be-6100-4cfe-a7db-4fb0a506b3ff.jpeg"
 
 
     ]
